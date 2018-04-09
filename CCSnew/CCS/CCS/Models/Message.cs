addbb57af204989c8fcbb2acf077babd6e8fcd34
JDub <contact@conquest-marketing.com>
2018-04-09 13:50:25 -0700

    proj and msg models start

---
+++ CCSnew/CCS/CCS/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCS.Models
+{
+    public class Message
+    {
+        int ID { get; set; }
+        int FromID { get; set; }
+        int ToID { get; set; }
+        string Text { get; set; }
+        Read Status { get; set; }
+    }
+
+    public enum Read
+    {
+       Read = 1,
+       Unread = 0
+    }
+}
---
+++ CCSnew/CCS/CCS/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCS.Models
+{
+    public class Project
+    {
+        string ID { get; set; }
+        string CustomerID { get; set; }
+        string Description { get; set; }
+        float Quote { get; set; }
+        float TotalDue { get; set; }
+        DateTime QuoteDate { get; set; }
+        DateTime InvoiceDue { get; set; }
+        Paid Paid { get; set; }
+
+    }
+
+    public enum Paid
+    {
+        Paid = 1,
+        Unaid = 2
+    }
+
+    public class Note
+    {
+        int ID { get; set; }
+        int From { get; set; }
+        int ProjID { get; set; }
+        string Text {get;set;}
+}
