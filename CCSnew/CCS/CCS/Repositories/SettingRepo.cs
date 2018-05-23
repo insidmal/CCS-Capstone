62fcd0e92d4f7a80197406c06850990ab63f7aee
JDub <contact@conquest-marketing.com>
2018-05-23 12:35:26 -0700

    settings completed

--- CCSnew/CCS/CCS/Repositories/SettingRepo.cs
+++ CCSnew/CCS/CCS/Repositories/SettingRepo.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using CCS.Models;
 
+// CREATIVE CYBER SOLUTIONS
+// CREATED 05/22/2018
+// CREATED BY JOHN BELL contact@conquest-marketing.com
+// UPDATED 05/23/2018
+// UPDATED BY JOHN BELL contact@conquest-marketing.com
+
+
 namespace CCS.Repositories
 {
     public class SettingRepo : ISettingRepository
@@ -15,11 +22,10 @@ namespace CCS.Repositories
         }
 
         public Settings GetSettings() => context.Settings.FirstOrDefault();
-        
 
         public Settings UpdateSettings(Settings s)
         {
-            context.Update(s);
+            context.Settings.Update(s);
             context.SaveChanges();
             return s;
         }
